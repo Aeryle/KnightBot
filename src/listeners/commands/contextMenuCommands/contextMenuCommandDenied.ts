46d7a597d8d1b622a7dbb131176fff5d12cf7b93
Aeryle <contact.aeryle@pm.me>
2025-09-18 17:01:26 +0200

    :rotating_light: use flags instead of the old ephemeral option

--- src/listeners/commands/contextMenuCommands/contextMenuCommandDenied.ts
+++ src/listeners/commands/contextMenuCommands/contextMenuCommandDenied.ts
@@ -1,5 +1,6 @@
 import type { ContextMenuCommandDeniedPayload, Events } from '@sapphire/framework'
 import { Listener, UserError } from '@sapphire/framework'
+import { MessageFlags } from 'discord.js'
 
 export class UserEvent extends Listener<typeof Events.ContextMenuCommandDenied> {
   public override async run(
@@ -20,7 +21,7 @@ export class UserEvent extends Listener<typeof Events.ContextMenuCommandDenied> 
     return interaction.reply({
       content,
       allowedMentions: { users: [interaction.user.id], roles: [] },
-      ephemeral: true,
+      flags: MessageFlags.Ephemeral,
     })
   }
 }
