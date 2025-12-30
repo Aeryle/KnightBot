0e17a40f4639d9fdb3a993112f808dc7738f1bf8
Aeryle <contact.aeryle@pm.me>
2025-12-30 23:40:00 +0100

    :bug: fix(ping-testers): allow the mod tester roles mention

--- src/commands/General/ping-testers.ts
+++ src/commands/General/ping-testers.ts
@@ -1,9 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators'
 import { Command } from '@sapphire/framework'
 import { envParseString } from '@skyra/env-utilities'
-import { InteractionContextType, MessageFlags } from 'discord.js'
-
-import { dev } from '$lib/constants'
+import { InteractionContextType } from 'discord.js'
 
 @ApplyOptions<Command.Options>({
   name: 'ping-testers',
@@ -26,7 +24,10 @@ export class PingTestersCommand extends Command {
 
     return interaction.reply({
       content: `<@&${envParseString('MOD_TESTER_ROLE_ID')}>`,
-      flags: dev ? MessageFlags.Ephemeral : [],
+      // flags: dev ? MessageFlags.Ephemeral : [],
+      allowedMentions: {
+        roles: [envParseString('MOD_TESTER_ROLE_ID')],
+      },
     })
   }
 }
